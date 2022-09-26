4bf307aeae354a309cc23324d36f9e5a2e3bf7c7
Lin.19211 <Lin.19211@LABI1-6PC23>
2022-09-26 08:20:18 +0200

    Aggiungere i file di progetto.

---
+++ lezione19-9-22/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace lezione19_9_22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+            letturafile();
+        }
+        public struct list
+        {
+            public int col0;
+            public string col1;
+            public string col2;
+            public int col3;
+            public double col4;
+            public double col5;
+            public double col6;
+            public string col7;
+            public double col8;
+        }
+        list[] lista = new list[30];
+        int conta_record = 0;
+        string filename = @"./dati.csv";
+        string line = "";
+        int recordLenght = 110;
+        
+
+        int colonne = 9;        
+        char end_of_line = '#';
+        int end_of_line_position = 109;
+        
+
+        public void letturafile()
+        {           
+            var sr = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            byte[] br = new byte[recordLenght];
+            BinaryReader reader = new BinaryReader(sr);
+            
+            string str = "";
+            string[] strsplit = new string[9];
+            bool check = false;
+            for (check = false; check == false; conta_record++)
+            {
+                br = reader.ReadBytes(recordLenght);
+                line = Encoding.ASCII.GetString(br, 0, br.Length);
+
+                check = string.IsNullOrEmpty(str);
+                //MessageBox.Show(check.ToString());
+
+                strsplit = str.Split(',');
+
+                lista[conta_record].col0 = Convert.ToInt32(strsplit[0]);
+                lista[conta_record].col1 = strsplit[1];
+                lista[conta_record].col2 = strsplit[2];
+                lista[conta_record].col3 = Convert.ToInt32(strsplit[3]);
+
+                check_double_dot(ref strsplit[4]);
+                lista[conta_record].col4 = Convert.ToDouble(strsplit[4]);
+                check_double_dot(ref strsplit[5]);
+                lista[conta_record].col5 = Convert.ToDouble(strsplit[5]);
+                check_double_dot(ref strsplit[6]);
+                lista[conta_record].col6 = Convert.ToDouble(strsplit[6]);
+
+                lista[conta_record].col7 = strsplit[7];
+
+                MessageBox.Show(strsplit[8]);
+
+                string ssr = strsplit[8].Substring(strsplit[8].IndexOf('#'));
+
+                MessageBox.Show(strsplit[8]);
+
+                check_double_dot(ref strsplit[8]);
+                lista[conta_record].col8 = Convert.ToDouble(strsplit[8]);
+
+                /*MessageBox.Show(lista[conta_record].col1);
+                MessageBox.Show(lista[conta_record].col2);
+                MessageBox.Show(lista[conta_record].col3.ToString());
+                MessageBox.Show(lista[conta_record].col4.ToString());
+                MessageBox.Show(lista[conta_record].col5.ToString());
+                MessageBox.Show(lista[conta_record].col6.ToString());
+
+                MessageBox.Show(lista[conta_record].col8.ToString());*/
+            }
+        }
+        public void check_double_dot(ref string str)    //lettura del file
+        {
+            char[] cr = str.ToCharArray();
+            for (int a = 0; a < str.Length; a++)
+            {
+                if (cr[a] == '.')
+                {
+                    cr[a] = ',';
+                }
+            }
+            str = new string(cr);
+        }
+
+        public void check_double_comma(ref string str)      //scrittura sul file
+        {
+            char[] cr = str.ToCharArray();
+            for (int a = 0; a < str.Length; a++)
+            {
+                if (cr[a] == ',')
+                {
+                    cr[a] = '.';
+                }
+            }
+            str = new string(cr);
+        }
+
+       
+    }
+}
